89bf7ef7c10dee0140262e57cf693e8c521538de
leksyking <foogundipe@student.oauife.edu.ng>
2022-11-03 02:09:54 +0100

    modify the codes

--- main.go
+++ main.go
@@ -4,14 +4,13 @@ import (
 	"encoding/csv"
 	"encoding/json"
 	"fmt"
-	"io"
 	"log"
 	"os"
 )
 
 // a struct for storing CSV lines and annotate with JSON struct field tags
 type CSVFileJSON struct {
-	SeriesNumber string `series_number`
+	SeriesNumber int    `json:"series_number"`
 	Filename     string `json:"filename"`
 	Name         string `json:"name"`
 	Description  string `json:"description"`
@@ -19,6 +18,41 @@ type CSVFileJSON struct {
 	Attributes   string `json:"attributes"`
 	UUID         string `json:"uuid"`
 	Hash         string `json:"hash"`
+}
+
+func GetLine(data [][]string) {
+	var rec CSVFileJSON
+	for j, record := range data {
+		//omit header line
+		if j > 0 {
+			for i, field := range record {
+				switch i {
+				case 0:
+					//rec.SeriesNumber =
+				case 1:
+					rec.Filename = field
+				case 2:
+					rec.Name = field
+				case 3:
+					rec.Description = field
+				case 4:
+					rec.Gender = field
+				case 5:
+					rec.Attributes = field
+				case 6:
+					rec.UUID = field
+				}
+
+			}
+			// convert the array to json using the MarshalIndent function
+			jsonData, err := json.MarshalIndent(rec, "", "  ")
+			if err != nil {
+				log.Fatal(err)
+			}
+			// add the data in filename.json
+			fmt.Println(string(jsonData))
+		}
+	}
 }
 
 func main() {
@@ -32,38 +66,11 @@ func main() {
 
 	// read csv values using csv.Reader
 	csvReader := csv.NewReader(f)
-	for {
-		record, err := csvReader.Read()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Fatal(err)
-		}
-		// convert the line to json
-		var rec CSVFileJSON
-		for i, field := range record {
-			switch i {
-			case 0:
-				rec.SeriesNumber = field
-			case 1:
-				rec.Filename = field
-			case 2:
-				rec.Name = field
-			case 4:
-				rec.Description = field
-			case 5:
-				rec.Gender = field
-			case 6:
-				rec.Attributes = field
-			case 7:
-				rec.UUID = field
-			}
-		}
-		jsonData, err := json.MarshalIndent(rec, "", "  ")
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(string(jsonData))
+
+	record, err := csvReader.ReadAll()
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	GetLine(record)
 }
