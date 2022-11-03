44363937158ffa0d7320db1aac15f63b9066565f
leksyking <foogundipe@student.oauife.edu.ng>
2022-11-03 09:49:11 +0100

    add more info inside READMe.md and add ConvertJSONToCSV function

--- README.md
+++ README.md
@@ -3,3 +3,32 @@
 Take the CSV provided by the teams, and generate a CHIP-0007 compatible json,
 calculate the sha256 of the json file and append it to each line in the csv as
 a (filename.output.csv)
+
+### main function
+- Open the csv file
+- Read all lines of the csv data
+- Send the data into the GetAllLines function
+
+### Create a struct to handle the csv data format
+
+### GetAllLines function
+- create a variable of an array of the struct
+- Range through the data while excluding the header
+- Create a variable to represent each rows of the data
+- Range through each rows and store each column under the respective header columns
+- Convert the row to json files
+- add the .json extension to the files
+- Open the json files and calculate sha256 on the json files
+- Append the hash to the json files
+- Create an output.json file from the array of various rows
+- Send the output.json file and the output.csv file to ConvertJSONToCSV function to create our updated csv file
+
+### ConvertJSONToCSV
+- Open the json file 
+- Decode the json file into an array of struct
+- Create the output.csv file
+- create a csv writer that writes to the csv file
+- Write the header row into the csv file
+- Range through the array of struct created and append the rows of the struct to each
+rows of an array of csvRow variable
+- write the array of csvRow into the csv writer
--- main.go
+++ main.go
@@ -23,11 +23,46 @@ type CSVFileJSON struct {
 	Hash         string `json:"hash"`
 }
 
-func GetLine(data [][]string) {
-	var rec CSVFileJSON
+func ConvertJSONToCSV(src, dest string) error {
+	srcFile, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer srcFile.Close()
+
+	var File []CSVFileJSON
+	if err := json.NewDecoder(srcFile).Decode(&File); err != nil {
+		return err
+	}
+	outputFile, err := os.Create(dest)
+	if err != nil {
+		return err
+	}
+	defer outputFile.Close()
+
+	writer := csv.NewWriter(outputFile)
+	defer writer.Flush()
+
+	header := []string{"Series Number", "Filename", "Name", "Description", "Gender", "Attributes", "UUID", "Hash"}
+	if err := writer.Write(header); err != nil {
+		return err
+	}
+	for _, file := range File {
+		var csvRow []string
+		csvRow = append(csvRow, file.SeriesNumber, file.Filename, file.Name, file.Description, file.Gender, file.Attributes, file.UUID, file.Hash)
+		if err := writer.Write(csvRow); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func GetAllLines(data [][]string) {
+	var JSONFile []CSVFileJSON
 	for j, record := range data {
 		//omit header line
-		if j > 0 && j < 3 {
+		if j > 0 {
+			var rec CSVFileJSON
 			for i, field := range record {
 				switch i {
 				case 0:
@@ -45,7 +80,6 @@ func GetLine(data [][]string) {
 				case 6:
 					rec.UUID = field
 				}
-
 			}
 			// convert the array to json using the MarshalIndent function
 			jsonData, err := json.MarshalIndent(rec, "", "  ")
@@ -61,14 +95,28 @@ func GetLine(data [][]string) {
 			if err != nil {
 				log.Fatal(err)
 			}
+			defer fn.Close()
 			h := sha256.New()
 			if _, err := io.Copy(h, fn); err != nil {
 				log.Fatal(err)
 			}
 			rec.Hash = hex.EncodeToString(h.Sum(nil))
 			// add the data in filename.json
-			fmt.Println(string(jsonData))
+			// fmt.Println(string(jsonData))
+			// convert back JSON to csv file
+			JSONFile = append(JSONFile, rec)
 		}
+	}
+	jsonData, err := json.MarshalIndent(JSONFile, "", "  ")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = os.WriteFile("output.json", jsonData, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := ConvertJSONToCSV("output.csv", "output.json"); err != nil {
+		log.Fatal(err)
 	}
 }
 
@@ -81,13 +129,12 @@ func main() {
 	// close the file at the end of the program
 	defer f.Close()
 
-	// read csv values using csv.Reader
+	// read csv data using csv.Reader
 	csvReader := csv.NewReader(f)
-
 	record, err := csvReader.ReadAll()
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	GetLine(record)
+	// grab all the csv data
+	GetAllLines(record)
 }
